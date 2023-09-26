f5f04c868b739be6a4a86b1841240d7c548e35ac
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2023-09-26 17:56:09 +0700

    Update stanford_dogs_dataloader.py

--- datasets/stanford_dogs_dataloader.py
+++ datasets/stanford_dogs_dataloader.py
@@ -1,20 +1,20 @@
 import torch
 import torchvision
 import os
-from ..configs.py import *
 
 dataset_dir = os.path.dirname(os.path.realpath(__file__)) + '/stanford_dogs/'
-train_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(IMAGE_SIZE),
-                                             torchvision.transforms.RandomHorizontalFlip(),
-                                             torchvision.transforms.RandomRotation(15),
-                                             torchvision.transforms.ToTensor(),
-                                             torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-test_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(IMAGE_SIZE),
-                                            torchvision.transforms.ToTensor(),
-                                            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-train_image_folder = torchvision.datasets.ImageFolder(root=dataset_dir+'train', transform = train_tfms)
-train_ds = torch.utils.data.DataLoader(train_image_folder, batch_size = BATCH_SIZE, shuffle=True, num_workers = 2)
-
-test_image_folder = torchvision.datasets.ImageFolder(root=dataset_dir+"test", transform = test_tfms)
-test_ds = torch.utils.data.DataLoader(test_image_folder, batch_size = BATCH_SIZE, shuffle=False, num_workers = 2)
+def create_dataloader(image_size, batch_size):
+    train_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(image_size),
+                                                 torchvision.transforms.RandomHorizontalFlip(),
+                                                 torchvision.transforms.RandomRotation(15),
+                                                 torchvision.transforms.ToTensor(),
+                                                 torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    test_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(image_size),
+                                                torchvision.transforms.ToTensor(),
+                                                torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+    train_image_folder = torchvision.datasets.ImageFolder(root=dataset_dir+'train', transform = train_tfms)
+    train_ds = torch.utils.data.DataLoader(train_image_folder, batch_size = batch_size, shuffle=True, num_workers = 2)
+    
+    test_image_folder = torchvision.datasets.ImageFolder(root=dataset_dir+"test", transform = test_tfms)
+    test_ds = torch.utils.data.DataLoader(test_image_folder, batch_size = batch_size, shuffle=False, num_workers = 2)
