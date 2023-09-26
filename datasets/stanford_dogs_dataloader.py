76f6dbbd5b34c05a14c0285e9fafc0f4390e0e42
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2023-09-26 15:05:05 +0700

    Create stanford_dogs_dataloader.py

---
+++ datasets/stanford_dogs_dataloader.py
@@ -0,0 +1,25 @@
+import os
+import torch
+import shutil
+import torch_geometric
+import torchvision
+import numpy as np
+
+BATCH_SIZE = 50
+IMAGE_SIZE = (224, 224)
+
+dataset_dir = "../datasets/stanford_dogs/"
+train_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(IMAGE_SIZE),
+                                             torchvision.transforms.RandomHorizontalFlip(),
+                                             torchvision.transforms.RandomRotation(15),
+                                             torchvision.transforms.ToTensor(),
+                                             torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+test_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(IMAGE_SIZE),
+                                            torchvision.transforms.ToTensor(),
+                                            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_image_folder = torchvision.datasets.ImageFolder(root=dataset_dir+'train', transform = train_tfms)
+train_ds = torch.utils.data.DataLoader(train_image_folder, batch_size = BATCH_SIZE, shuffle=True, num_workers = 2)
+
+test_image_folder = torchvision.datasets.ImageFolder(root=dataset_dir+"test", transform = test_tfms)
+test_ds = torch.utils.data.DataLoader(test_image_folder, batch_size = BATCH_SIZE, shuffle=False, num_workers = 2)
