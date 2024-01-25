810b248669fa10a778fbb679be48c3c55cea685c
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2024-01-25 15:03:36 +0700

    Update stanford_dogs_dataloader.py

--- datasets/stanford_dogs_dataloader.py
+++ datasets/stanford_dogs_dataloader.py
@@ -1,8 +1,44 @@
 import torch
 import torchvision
 import os
+import scipy
 
 dataset_dir = os.path.dirname(os.path.realpath(__file__)) + '/stanford_dogs/'
+
+train_list = [f[0][0] for f in scipy.io.loadmat(dataset_dir + 'train_list.mat')['file_list']]
+test_list = [f[0][0] for f in scipy.io.loadmat(dataset_dir + 'test_list.mat')['file_list']]
+
+if os.path.exists(dataset_dir + 'dataset'):
+    shutil.rmtree(dataset_dir + 'dataset')
+os.mkdir(dataset_dir + 'dataset')
+os.mkdir(dataset_dir + 'dataset/train')
+os.mkdir(dataset_dir + 'dataset/test')
+for name in os.listdir(dataset_dir + 'Images'):
+    os.mkdir(dataset_dir + 'dataset/train/' + name)
+    os.mkdir(dataset_dir + 'dataset/test/' + name)
+
+for name in tqdm(train_list):
+    raw_img = Image.open(dataset_dir + 'Images/' + name)
+    with open(dataset_dir + 'Annotation/' + name[:-4], 'r') as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+    cropped_img = raw_img.crop((int(soup.xmin.contents[0]),
+                                int(soup.ymin.contents[0]),
+                                int(soup.xmax.contents[0]),
+                                int(soup.ymax.contents[0])))
+    cropped_img.convert('RGB').save(dataset_dir + 'dataset/train/' + name)
+
+for name in tqdm(test_list):
+    raw_img = Image.open(dataset_dir + 'Images/' + name)
+    with open('Annotation/' + name[:-4], 'r') as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+    cropped_img = raw_img.crop((int(soup.xmin.contents[0]),
+                                int(soup.ymin.contents[0]),
+                                int(soup.xmax.contents[0]),
+                                int(soup.ymax.contents[0])))
+    cropped_img.convert('RGB').save(dataset_dir + 'dataset/test/' + name)
+
 def create_dataloader(image_size, batch_size):
     train_tfms = torchvision.transforms.Compose([torchvision.transforms.Resize(image_size),
                                                  torchvision.transforms.RandomHorizontalFlip(),
